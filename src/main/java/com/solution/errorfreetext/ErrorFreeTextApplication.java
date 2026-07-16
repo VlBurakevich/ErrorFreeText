f18ecdc5b30430f193c2a76b52eb3826040f7ad7
Burakevich Vladislav <burakevichvalli040@gmail.com>
2026-07-16 11:26:30 +0300

    initial commit

---
+++ src/main/java/com/solution/errorfreetext/ErrorFreeTextApplication.java
@@ -0,0 +1,13 @@
+package com.solution.errorfreetext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrorFreeTextApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErrorFreeTextApplication.class, args);
+    }
+
+}
